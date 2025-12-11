2730c00db101d1d2bef8f25e7efbbac187c67296
Tamal <ckbtamaldipnew@gmail.com>
2025-12-11 20:20:57 +0600

    created all router which has all the crud functionalities

--- todo/apps/backend/src/todos/todo.router.ts
+++ todo/apps/backend/src/todos/todo.router.ts
@@ -1,4 +1,61 @@
-import { Router } from "nestjs-trpc";
+import { Input, Mutation, Query, Router } from "nestjs-trpc";
+import { TodosService } from "./todos.service";
+import { z } from 'zod';
+import { createTodoSchema, todoSchema } from "./todos.schema";
+import type { CreateTodoInput } from "./todos.schema";
 
 @Router({ alias: 'todo' })
-export class TodoRouter { }
+    
+//! This router has full crud functionality and the logics are getting called from service
+export class TodoRouter {
+    constructor(private readonly todosService: TodosService) { }
+
+    @Query({
+        input: z.object({ id: z.string() }),
+        output: todoSchema,
+    })
+    getTodoById(@Input('id') id:string) {
+        return this.todosService.getTodoById(id);
+    }
+
+    @Query({
+        output: z.array(todoSchema),
+    })
+    getAllTodos() {
+        return this.todosService.getAllTodos();
+    }
+
+    @Mutation({
+        input: createTodoSchema,
+        output: todoSchema
+    })
+    createTodo(@Input() todoData: CreateTodoInput) {
+        return this.todosService.createTodo(todoData);
+    }
+
+    @Mutation({
+        input: z.object({
+            id: z.string(),
+            data: createTodoSchema.partial()
+        }),
+        output: todoSchema
+    })
+    updateTodo(
+        @Input('id') id: string,
+        @Input('data') data: Partial<CreateTodoInput>
+    ) {
+        return this.todosService.updateTodo(id, data);
+    }
+
+    @Mutation({
+        input: z.object({
+            id: z.string(),
+        }),
+        output: z.boolean()
+    })
+    deleteTodo(
+        @Input('id') id: string
+    ) {
+        return this.todosService.deleteTodo(id);
+    }
+ }
--- todo/apps/backend/src/todos/todos.schema.ts
+++ todo/apps/backend/src/todos/todos.schema.ts
@@ -10,4 +10,11 @@ export const todoSchema = z.object({
     priority: z.enum(['low', 'medium', 'high']).optional(),
 });
 
+export const createTodoSchema = todoSchema.omit({
+    id: true,
+    createdAt: true
+});
+
+export type CreateTodoInput = z.infer<typeof createTodoSchema>;
+
 export type Todo = z.infer<typeof todoSchema>;
